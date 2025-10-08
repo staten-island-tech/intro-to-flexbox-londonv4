06052ff905be34339504ef9d13809380882560ed
londonv4 <londonv4@nycstudents.net>
2025-10-08 09:32:21 -0700

    j

---
+++ app.js
@@ -0,0 +1,10 @@
+const makeup = [
+{
+    name: "Eternal Sunshine",
+    category: Gloss,
+    instock: true, 
+    price: 14.99,
+    img: "img/pinklipgloss",
+    alt: "5ml pink gloss for lips",
+}
+]
