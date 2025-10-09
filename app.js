43d98b168e6ade79706772f351fefc2a9249d55c
londonv4 <londonv4@nycstudents.net>
2025-10-09 09:34:19 -0700

    ii

--- app.js
+++ app.js
@@ -8,3 +8,15 @@ const makeup = [
     alt: "5ml pink gloss for lips",
 }
 ]
+
+// create inject function
+function inject(item) {
+    //do something
+    let name= testing
+    const container = document.querySelector(".container");
+    container.insertAdjacentHTML("afterbegin", '<h1> {testing} </h1>');
+    //query the container
+    //using adjacent html push card into container
+}
+inject(products[0]);
+//loop through items
